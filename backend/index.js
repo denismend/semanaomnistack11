d3cc54a2781b8313aad26ee8cdd8da1e3d695e28
denismend <denisladeira1@gmail.com>
2020-03-23 16:53:33 -0300

    express lib

--- backend/index.js
+++ backend/index.js
@@ -2,5 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => {
+    //return response.send('Hello World !');
+    return response.json( {
+        evento: 'semanaonmistack 11.0',
+        aluno: 'Denis Ladeira'
+    } );
+});
+
 app.listen(3333);
 
