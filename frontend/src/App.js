eeb1b3e059d29c40679dfa5bb977cde1ccd30497
denismend <denisladeira1@gmail.com>
2020-03-26 18:16:58 -0300

    state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Header from './Header';
+
+// JSX -- Javascript / XML
 
 function App() {
+  let [count, setCounter] = useState(0);
+  // Array [valor, funcao de atualização]
+
+  function increment() {
+    setCounter(count + 1);
+  }
+
   return (
-    <h1>Hello World</h1>
+    //<Header title="Semana OmniStack" />
+    <div>
+      <Header>
+        Contador: { count }
+      </Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
