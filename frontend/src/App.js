716ffb9b69a9047cd6a4bcacf0c04963dc7b0239
denismend <denisladeira1@gmail.com>
2020-03-27 12:00:45 -0300

    logon page and styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Header from './Header';
+import './global.css';
+
+//import Header from './Header';
+import Logon from './pages/Logon';
 
 // JSX -- Javascript / XML
 
 function App() {
+  /*
   let [count, setCounter] = useState(0);
   // Array [valor, funcao de atualização]
 
   function increment() {
     setCounter(count + 1);
   }
+  */
 
   return (
     //<Header title="Semana OmniStack" />
+    /*
     <div>
       <Header>
         Contador: { count }
       </Header>
       <button onClick={increment}>Incrementar</button>
     </div>
+    */
+    <Logon />
   );
 }
 
