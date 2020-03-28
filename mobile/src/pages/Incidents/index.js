5ce1e5836db8831a0832b236ffe95b256cb8fb6e
denismend <denisladeira1@gmail.com>
2020-03-28 15:20:59 -0300

    mobile finished. back fix

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,25 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.png';
 
 import styles from './styles';
 
 export default function Incidents() {
+    const [incidents, setIncidents] = useState([]);
+    const [total, setTotal] = useState(0); 
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+
     const navigation = useNavigation();
 
-    function navigateToDatails() {
-        navigation.navigate('Detail');
+    function navigateToDatails(incident) {
+        navigation.navigate('Detail', { incident });
     }
+
+    async function loadIncidents() {
+        if(loading) {
+            return;
+        }
+
+        if(total > 0 && incidents.length === total) {
+            return;
+        } 
+
+        setLoading(true);
+
+        const response = await api.get('incidents', {
+            params: { page }
+        });
+
+        setLoading(false);
+        setPage(page + 1);
+
+        setIncidents([...incidents, ...response.data]);
+        setTotal(response.headers['x-total-count']);
+    }
+
+    useEffect(() => {
+        loadIncidents();
+    }, []);
 
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg} /> 
                 <Text style={styles.headerText} > 
-                    Total de <Text style={styles.headerTextBold}>0 Casos</Text>.
+                    Total de <Text style={styles.headerTextBold}>{total} Casos</Text>.
                 </Text>
             </View>
 
@@ -27,24 +60,31 @@ export default function Incidents() {
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
 
             <FlatList 
-                data={['1', '2', '3', '4', '5']}
+                data={incidents}
                 style={styles.incidentList}
-                keyExtractor={incident => incident}
+                keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
-                renderItem={() => (
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
+                renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
-                        <Text style={styles.incidentValue}>APAD</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
 
                         <Text style={styles.incidentProperty}>CASO:</Text>
-                        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
 
                         <Text style={styles.incidentProperty}>VALOR:</Text>
-                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+                        <Text style={styles.incidentValue}>
+                            {Intl.NumberFormat('pr-BR', { 
+                                style: 'currency', 
+                                currency: 'BRL'
+                            }).format(incident.value)}
+                        </Text>
 
                         <TouchableOpacity 
                             style={styles.detailsButton} 
-                            onPress={navigateToDatails}
+                            onPress={() => {navigateToDatails(incident)}}
                         >
                             <Text style={styles.detailsButtonText}>Ver mais detalhes</Text> 
                             <Feather name="arrow-right" size={16} color="#E02041" />
